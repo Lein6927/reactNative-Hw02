499e953450465114e434f430e212dc9269a7e810
Lein6927 <lightnine861108@gmail.com>
2023-05-08 10:47:15 +0800

    add 復原

--- App.js
+++ App.js
@@ -1,39 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Navigator,Image} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
-
-
-
-
-
-  
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: 'https://images.plurk.com/3v2pCeK4ABEs7wg3dPjZwI.png' }}></Image>
-      <Text style='fontSize:30'>換頁</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-
-
-
-
-//style
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  image: {
-    width: 240,
-    height: 180,
-    resizeMode: 'stretch',
-    marginLeft: 20,
   },
 });
